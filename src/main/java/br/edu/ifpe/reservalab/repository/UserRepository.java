325d08707e079471d81c4deb648657f0d5129f5e
Luiz Eduardo <57048776+luizcodedthat@users.noreply.github.com>
2026-03-23 22:49:00 -0300

    feat: cria repositorios de laboratorio e usuario

---
+++ src/main/java/br/edu/ifpe/reservalab/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package br.edu.ifpe.reservalab.repository;
+
+import br.edu.ifpe.reservalab.model.User;
+import br.edu.ifpe.reservalab.enums.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    List<User> findAllByRole(UserRole role);
+}
