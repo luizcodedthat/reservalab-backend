b6785356692498fe97b015db00594ee2dce25be4
Luiz Eduardo <57048776+luizcodedthat@users.noreply.github.com>
2026-05-04 17:04:23 -0300

    feat: implementa fluxo de voto

--- src/main/java/br/edu/ifpe/reservalab/repository/CommentVoteRepository.java
+++ src/main/java/br/edu/ifpe/reservalab/repository/CommentVoteRepository.java
@@ -1,6 +1,7 @@
 package br.edu.ifpe.reservalab.repository;
 
 import br.edu.ifpe.reservalab.dto.CommentVoteSummary;
+import br.edu.ifpe.reservalab.dto.UserCommentVote;
 import br.edu.ifpe.reservalab.model.CommentVote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentVoteRepository extends JpaRepository<CommentVote, Long> {
@@ -25,4 +27,30 @@ public interface CommentVoteRepository extends JpaRepository<CommentVote, Long> 
     List<CommentVoteSummary> findVoteSummariesByCommentIds(
             @Param("commentIds") List<Long> commentIds
     );
+
+    @Query("""
+    SELECT cv
+    FROM CommentVote cv
+    WHERE cv.user.id = :userId
+      AND cv.comment.id = :commentId
+""")
+    Optional<CommentVote> findVote(
+            @Param("userId") Long userId,
+            @Param("commentId") Long commentId
+    );
+
+    @Query("""
+        SELECT new br.edu.ifpe.reservalab.dto.UserCommentVote(
+            cv.comment.id,
+            cv.voteType
+        )
+        FROM CommentVote cv
+        WHERE cv.user.id = :userId
+          AND cv.comment.id IN :commentIds
+        """)
+    List<UserCommentVote> findVotesByUserAndCommentIds(
+            @Param("userId") Long userId,
+            @Param("commentIds") List<Long> commentIds
+    );
+
 }
