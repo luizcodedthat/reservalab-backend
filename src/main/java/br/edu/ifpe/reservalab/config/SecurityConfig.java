7d07bcb6617d82358e479726d35d8058ec182921
Luiz Eduardo <57048776+luizcodedthat@users.noreply.github.com>
2026-05-05 03:37:30 -0300

    fix: remove config de cors duplicada

--- src/main/java/br/edu/ifpe/reservalab/config/SecurityConfig.java
+++ src/main/java/br/edu/ifpe/reservalab/config/SecurityConfig.java
@@ -65,7 +65,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://reservalab-frontend.netlify.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
